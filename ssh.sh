df4151787b96e3a5e07f48fd8c1b2c3558f4ed21
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-02-15 11:58:12 +0200

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,6 @@
+ssh-keygen
+ssh-copy-id [username]@[machine_ip]
+# It requires a password to copy the key
+# but once the key is copied the login 
+# doesn't require any password
+ssh [username]@[hostname]

5b462743bd4c0db0f303670b312628dbc436dd4b
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-02-15 20:25:14 +0200

    Update 7_yum_repo.sh

--- 7_yum_repo.sh
+++ 7_yum_repo.sh
@@ -2,12 +2,19 @@ yum install -y tmux
 
 yum install -y httpd mysql
 
-mkdir /var/www/html/zabbix
-cd /var/www/html/zabbix
-wget -q -r -t1 --no-parent -nd --mirror https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64
+cd /var/www/html
+yum install -y wget
+wget -r --no-parent --mirror https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64
+mv /var/www/html/repo.zabbix.com/ /var/www/html/
 yum install -y yum-utils createrepo
-createrepo --update /var/www/html/zabbix/zabbix/4.4/rhel/7/x86_64
-yum-config-manager --add-repo http://<my_id>/zabbix
+createrepo --update /var/www/html/zabbix/4.4/rhel/7/x86_64
+yum-config-manager --add-repo http://<host_ip>/zabbix/4.4/rhel/7/x86_64
+
+cd /etc/yum.repos.d/
+# Here you find the repo files, for any repo
+# you can disable it by setting enabled=0
+# Alternativly you can use yum-config-manager --disable
+
 yum-config-manager --disable {base,extras,updates}
 
 yum install -y zabbix zabbix-web php zabbix-server zabbix-agent

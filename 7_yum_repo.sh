39d68ba2cf25f399e30b95da6bf82f4865e81639
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-02-15 15:22:50 +0200

    Update 7_yum_repo.sh

--- 7_yum_repo.sh
+++ 7_yum_repo.sh
@@ -2,4 +2,7 @@ yum install -y tmux
 
 yum install -y httpd mysql
 
-# Incomplete...
+mkdir /var/www/html/zabbix
+cd /var/www/html/zabbix
+wget -r --no-parent https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64
+yum install -y yum-utils createrepo

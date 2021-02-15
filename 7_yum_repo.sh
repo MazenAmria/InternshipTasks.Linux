aa12caab989a5c2370b9e628b94f38a447e4cd9e
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-02-15 16:26:35 +0200

    Update 7_yum_repo.sh

--- 7_yum_repo.sh
+++ 7_yum_repo.sh
@@ -4,5 +4,10 @@ yum install -y httpd mysql
 
 mkdir /var/www/html/zabbix
 cd /var/www/html/zabbix
-wget -r --no-parent https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64
+wget -q -r -t1 --no-parent -nd --mirror https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64
 yum install -y yum-utils createrepo
+createrepo --update /var/www/html/zabbix/zabbix/4.4/rhel/7/x86_64
+yum-config-manager --add-repo http://<my_id>/zabbix
+yum-config-manager --disable {base,extras,updates}
+
+yum install -y zabbix zabbix-web php zabbix-server zabbix-agent

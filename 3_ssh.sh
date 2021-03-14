f13109fddd85dea10af6098f4a1a8bbd21aeeaff
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-14 11:36:46 +0200

    Update 3_ssh.sh
    Key won't be regenerated now.
    Host is given as an argument to the script.

--- 3_ssh.sh
+++ 3_ssh.sh
@@ -1,6 +1,16 @@
-ssh-keygen
-ssh-copy-id [username]@[machine_ip]
+#!/bin/bash
+
+key=~/.ssh/id_rsa.pub
+
+if [ ! -f "$key" ]; then
+    ssh-keygen
+else
+    echo "Public SSH key exists"
+fi
+
+ssh-copy-id $1
 # It requires a password to copy the key
 # but once the key is copied the login 
 # doesn't require any password
-ssh [username]@[hostname]
+
+ssh $1

1fd2cf06485941ea2e8481af3d9c03577a9c2697
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-14 10:55:36 +0200

    LVM: mount is now permanent

--- 1_LVM.sh
+++ 1_LVM.sh
@@ -1,6 +1,23 @@
+#!/bin/bash
+
+# create a volume group with extent size 16M
 vgcreate -s 16M vgrp /dev/sdb1
+
+# create logical volume with 50 extents
 lvcreate -l 50 vgrp -n lvol
+
+# create ext4 filesystem with the logical volume
 mkfs.ext4 /dev/vgrp/lvol
-mount /dev/vgrp/lvol /mnt/data
-mkdir /mnt/data
-mount /dev/vgrp/lvol /mnt/data
+
+# mount destination
+mnt_dest="/mnt/data"
+
+# create the destination (and its parents) if not exist
+mkdir -p $(mnt_dest)
+
+# extract the uuid of the logical volume
+uuid=$(blkid | grep lvol | cut -d" " -f2)
+
+# write it to /etc/fstab
+# <file system>	<mount point>	<type>	<options>	<dump>	<pass>
+printf '%s\t%s\text4\tdefaults\t0\t0\n' $uuid $mnt_dest >> /etc/fstab

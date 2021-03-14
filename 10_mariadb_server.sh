1f53e101285710f8e96571db610e11dfd162fdfe
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-14 14:15:52 +0200

    Update 10_mariadb_server.sh

--- 10_mariadb_server.sh
+++ 10_mariadb_server.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+# install mariadb server and start it
 yum install -y mariadb-server
 systemctl start mariadb
 
+# open the db port (3306)
 iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+# make changes permanent
+iptables-save > /etc/sysconfig/iptables
 
+# create the database
 mysql -e "CREATE DATABASE studentdb;"
-mysql -e "CREATE USER user1@localhost IDENTIFIED BY 'mariadb';"
-mysql -e "GRANT ALL PRIVILEGES ON studentdb.* TO 'user1'@'localhost';"
+
+# create the user
+mysql -e "CREATE USER user1@192.168.56.3 IDENTIFIED BY 'mariadb';"
+
+# handle privileges
+mysql -e "GRANT ALL PRIVILEGES ON studentdb.* TO 'user1'@'192.168.56.3';"
 mysql -e "FLUSH PRIVILEGES;"

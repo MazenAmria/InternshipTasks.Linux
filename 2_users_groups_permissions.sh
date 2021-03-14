f8dde43d28dd6c62e7da3c1210a652c52b885398
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-14 11:06:59 +0200

    Update 2_users_groups_permissions.sh

--- 2_users_groups_permissions.sh
+++ 2_users_groups_permissions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 useradd user1 --uid 601 --password redhat --shell /sbin/nologin
 
 groupadd TrainingGroup
@@ -5,6 +7,7 @@ usermode user1 -aG TrainingGroup
 
 useradd user2 --password redhat
 useradd user3 --password redhat
+
 groupadd admin
 usermod user2 -aG admin
 usermod user3 -aG admin,wheel

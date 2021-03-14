42ba233b089ee2a966fdaaae862a42c89dca2443
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-14 11:56:36 +0200

    Update 5_permissions.sh

--- 5_permissions.sh
+++ 5_permissions.sh
@@ -1,2 +1,7 @@
-vim /etc/selinux/config
-# add/modify the line to be SELINUX=enforcing
+#!/bin/bash
+
+# remove any line starts with 'SELINUX=' if exists
+sed -i 's/^SELINUX=.*$//' /etc/selinux/config
+
+# append the line 'SELINUX=enforcing' to get the enforcing mode
+echo 'SELINUX=enforcing' >> /etc/selinux/config

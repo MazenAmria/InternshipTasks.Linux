eb464408c232ab56c4fdd22319a6901f06e4ad9b
MazenAmria <69749730+MazenAmria@users.noreply.github.com>
2021-03-14 11:39:50 +0200

    Update 4_permissions.sh

--- 4_permissions.sh
+++ 4_permissions.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
 cp /etc/fstap /var/tmp/admin
+
+# Make user1 as the owner
 chown user1:user1 /var/tmp/admin
-chmod 644 /var/tmp/admin
+
+# Owner (user1) can read and write
+# Group (TrainingGroup) can read only
+# Others (user2 for example) have no permissions
+chmod 640 /var/tmp/admin

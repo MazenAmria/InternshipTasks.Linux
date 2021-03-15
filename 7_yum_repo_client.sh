08f5afe10039d506dc141940f7f64701fdc07a86
root <root@localhost.localdomain>
2021-03-15 09:23:40 +0000

    Creating local repo

---
+++ 7_yum_repo_client.sh
@@ -0,0 +1,16 @@
+# create the repo
+yum install -y createrepo
+createrepo --update /var/www/html/repo.zabbix.com/
+
+# add the repo (and enable it)
+yum-config-manager --add-repo http://192.168.56.4/
+
+# cleaning the cache
+yum clean all
+
+# the repo config files are stored ad /etc/yum.repos.d/
+# for any repo you can disable it by setting enabled=0
+# alternativly you can use yum-config-manager --disable <repo>
+
+# installing the following packages from the local repo
+yum install -y zabbix-server-mysql zabbix-web-mysql zabbix-agent zabbix-java-gateway
---
+++ 7_yum_repo_server.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# install tmux
+yum install -y tmux
+
+# install httpd and mysql
+yum install -y httpd mysql
+
+# start httpd to share the repo locally
+systemctl start http
+
+# import the GPG key for Zabbix repo
+rpm --import https://repo.zabbix.com/RPM-GPG-KEY-ZABBIX-A14FE591
+
+# install Zabbix release to create the repo
+rpm -Uv https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
+
+# now on yum repolist we see zabbix repo has been created
+# zabbix/x86_64                   Zabbix Official Repository - x86_64
+# zabbix-non-supported/x86_64     Zabbix Official Repository non-supported - x86_64
+
+mkdir -p /var/www/html/repos/{zabbix,zabbix-non-supported}
+
+# fetch the repo to the local server using reposync
+reposync \
+        --gpgcheck --plugins \
+        --delete --newest-only \
+        --repoid=zabbix \
+        --download-metadata --downloadcomps \
+        --download_path=/var/www/html/repos/
+
+reposync \
+        --gpgcheck --plugins \
+        --delete --newest-only \
+        --repoid=zabbix-non-supported \
+        --download-metadata --downloadcomps \
+        --download_path=/var/www/html/repos/
+
+# create the repo
+yum install -y yum-utils createrepo
+createrepo /var/www/html/
